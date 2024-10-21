256acbc913a598f4561ca072d0ce90a648f475ab
harshit mahajan <harshit388.be22@chitkara.edu.in>
2024-10-21 15:32:45 +0530

    21-10-2024

--- server/server.js
+++ server/server.js
@@ -1,1 +1,23 @@
-// this is server.js
+const express = require ("express")
+const connectDb = require("./config/dbConnection")
+const errorHandler = require("./middleware/errorHandler")
+const cors = require("cors")
+
+connectDb()
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(express.json())
+app.use(cors())
+
+
+app.get("/", (req,res)=>{
+    res.send("Hello World")
+})
+
+app.use(errorHandler)
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`)
+})
+
